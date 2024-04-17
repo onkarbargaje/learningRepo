21de935b88fa3a387b504185af2d9e66578d33e6
onkarbargaje <bargajeonkar99@gmail.com>
2024-04-17 16:42:28 +0530

    added 2 files

---
+++ src/main/RegistrationPage.java
@@ -0,0 +1,11 @@
+package main;
+
+public class RegistrationPage {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is registration page");
+
+	}
+
+}
---
+++ src/test/RoughTest.java
@@ -0,0 +1,12 @@
+package test;
+
+public class RoughTest {
+
+	public static void main(String[] args)
+	{
+
+        System.out.println("This is for rough work");
+
+	}
+
+}
