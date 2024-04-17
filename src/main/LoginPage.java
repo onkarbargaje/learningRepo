c728bb76013fc3027e49d74185c4d20442ac9606
onkarbargaje <bargajeonkar99@gmail.com>
2024-04-17 13:00:24 +0530

    added/updated some file

--- src/main/LoginPage.java
+++ src/main/LoginPage.java
@@ -5,6 +5,9 @@ public class LoginPage {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is my LoginPage");
+		String un= "Abcd@1234";
+		String pwd= "passwqwer";
+		System.out.println("user is logged in....");
 	}
 
 }
---
+++ src/main/SearchPage.java
@@ -0,0 +1,11 @@
+package main;
+
+public class SearchPage {
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is search page");
+
+	}
+
+}
